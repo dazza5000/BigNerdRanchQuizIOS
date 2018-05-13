9407470d9255b1f67b1bfedfa22e75727653a789
Darran Kelinske <darran.kelinske@gmail.com>
2018-05-13 18:35:32 -0500

    connect labels and add methods for buttons

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -11,5 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UiLabel!
     @IBOutlet var answerLabel: UiLabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton ) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton ) {
+        
+    }
 }
 
