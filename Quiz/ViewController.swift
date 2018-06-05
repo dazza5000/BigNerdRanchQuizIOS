b8e22cd328c8f3adb250ba778a08d5a053338f95
Darran Kelinske <darran.kelinske@gmail.com>
2018-06-04 20:17:41 -0500

    further work on animations

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -49,8 +49,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        
+        updateOffScreenLabel()
     }
-    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -59,18 +60,33 @@ class ViewController: UIViewController {
     }
     
     func animateLabelTransitions() {
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
         UIView.animate(withDuration: 0.5,
                        delay: 0,
-                       options: [],
+                       options: [.curveLinear],
                        animations: {
                             self.currentQuestionLabel.alpha = 0
                             self.nextQuestionLabel.alpha = 1
+                        
+                        self.view.layoutIfNeeded()
         },
                        completion: {_ in
                         swap(&self.currentQuestionLabel,
                              &self.nextQuestionLabel)
+                        swap(&self.currentQuestionLabelCenterXConstraint,
+                             &self.nextQuestionLabelCenterXConstraint)
                         
+                        self.updateOffScreenLabel()
+
         })
+    }
+    
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
 }
 
